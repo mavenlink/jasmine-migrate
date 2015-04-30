a1ad9eb1fa2535fd902d676800216d81bd92eabe
Jamie Rolfs <jamie@mavenlink.com>
2015-04-30 15:57:41 -0700

    Add specs for spy call control method proxies

--- test/jasmine-migrate.js
+++ test/jasmine-migrate.js
@@ -1,14 +1,63 @@
 'use strict';
 
-var jasmine = require('jasmine-core');
-var JasmineMigrate = require('../src/index.js');
+var _               = require('lodash');
+var jasmineCore     = require('jasmine-core');
+var JasmineMigrate  = require('../src/index.js');
+
+var jasmine = jasmineCore.jasmine;
+
+
+function jasmineWrapper(spec) {
+  jasmineCore.describe('describe', function() {
+    jasmineCore.it('it', function() {
+      spec.call(this);
+    });
+  }).execute();
+}
 
 
 describe('JasmineMigrate', function () {
-  var plugin;
 
-  beforeEach(function () {
-    plugin = new JasmineMigrate(jasmine);
-  });
+  describe('Jasmine 2 "Emulation"', function() {
+
+    describe('new spy syntax', function() {
+      var oldSyntax = ['andCallThrough', 'andCallFake', 'andThrow', 'andReturn'];
+      var newSyntax = ['callThrough', 'callFake', 'throwError', 'returnValue'];
+      var syntaxMap = _.zipObject(oldSyntax, newSyntax);
+      var test;
+
+      beforeEach(function () {
+        test = {
+          method: function () {
+            return 'original';
+          }
+        };
+      });
+
+      var itProxiesMethod = function (newMethod, oldMethod) {
+        it('proxies `and.' + newMethod + '` to `' + oldMethod + '`', function() {
+
+          sinon.spy(jasmine.Spy.prototype, oldMethod);
+
+          new JasmineMigrate(jasmine);
+
+          jasmineWrapper(function() {
+            // Catch error thrown by `andThrow` :)
+            try { 
+              var spy = jasmineCore.spyOn(test, 'method').and[newMethod]();
+
+              test.method();
+
+            } catch (error) {}
+          });
+
+          jasmine.Spy.prototype[oldMethod].should.have.been.called;
+        });
+      }; 
+
+      _.forIn(syntaxMap, itProxiesMethod);
+    });
+
+   }); 
 
 });
