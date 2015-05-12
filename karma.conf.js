8e0641b63abbc34404986cee7e9db0443e9a9a70
Jamie Rolfs <jamie@mavenlink.com>
2015-05-12 13:16:41 -0700

    Remove PhantomJS launcher

--- karma.conf.js
+++ karma.conf.js
@@ -15,19 +15,11 @@ module.exports = function (config) {
       'karma-sinon-chai',
       'karma-chrome-launcher',
       'karma-firefox-launcher',
-      'karma-phantomjs-launcher',
       'karma-mocha-reporter',
       'karma-browserify'
     ],
 
     browsers: ['Chrome'],
-
-    customLaunchers: {
-      'PhantomJS-debug': {
-        base: 'PhantomJS',
-        flags: ['--remote-debugger-port=9000']
-      }
-    },
 
     reporters: ['mocha'],
 
